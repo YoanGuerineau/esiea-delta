3cdb381c41d8110851842bcd77e09c8cfa523bad
YoanGuerineau <yoan.guerineau@live.fr>
2022-03-06 13:19:01 +0100

    Initial commit

---
+++ backend/src/main/java/com/esiea/esieadelta/EsieaDeltaApplication.java
@@ -0,0 +1,13 @@
+package com.esiea.esieadelta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsieaDeltaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsieaDeltaApplication.class, args);
+	}
+
+}
