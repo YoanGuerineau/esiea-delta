2b64212e23cf721f3155d7a0fd345db8b805862f
YoanGuerineau <yoan.guerineau@live.fr>
2022-03-10 23:00:48 +0100

    Implement models

--- backend/src/main/java/com/esiea/esieadelta/model/Article.java
+++ backend/src/main/java/com/esiea/esieadelta/model/Article.java
@@ -1,5 +1,81 @@
 package com.esiea.esieadelta.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table( name = "articles" )
 public class Article {
 
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
+	@Column( name = "article_id" )
+	private Integer id;
+	private String title;
+	private String author;
+	@DateTimeFormat( pattern = "yyyy-MM-dd" )
+	private Date date;
+	private String content;
+	@ManyToMany( mappedBy = "articles" )
+	private List<Category> categories = new ArrayList<>();
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public List<Category> getCategories() {
+		return categories;
+	}
+	
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+	
 }
