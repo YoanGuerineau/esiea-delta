436c0a24bdd1f8e3b37ae5106d907bbf65b69d20
BenoitEirik <olafsergent@gmail.com>
2022-03-13 18:24:02 +0100

    Add search bar

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,20 +2,32 @@ import React from 'react';
 import {
     Box,
     Heading,
-    VStack
+    VStack,
+	InputGroup,
+	InputLeftElement,
+	Input,
+	Spacer
 } from "@chakra-ui/react";
 import ArticleCard from '../components/article-card';
+import { SearchIcon } from '@chakra-ui/icons'
 
 function Home() {
 	return (
 		<Box>
-			<VStack>
+			<VStack spacing={4}>
 				<Heading
 					m={2}
 					fontSize="7xl"
 				>
 					Delta Blog
 				</Heading>
+				<InputGroup>
+  				  <InputLeftElement
+  				    pointerEvents='none'
+  				    children={<SearchIcon color='gray.300' />}
+  				  />
+  				  <Input type='search' placeholder='Recherche...' />
+  				</InputGroup>
 				/* articles, static */
 				<ArticleCard />
 				<ArticleCard />
